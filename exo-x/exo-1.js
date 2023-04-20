b64f4ed1546888397398ee93db45eb9cb7d06c43
ehossard <etiennehossard@gmail.com>
2023-04-20 08:46:11 +0200

    exo-2

--- exo-x/exo-1.js
+++ exo-x/exo-1.js
@@ -10,3 +10,4 @@ class Avion {
 }
 
 const avion1 = new Avion('Airbus', 'a380', 100, 700, 20000, 900);
+
---
+++ exo-x/exo-2.js
@@ -0,0 +1,57 @@
+class Avion {
+    constructor(marque, modele, longueur, nbPlace, reservoir, vitesse) {
+      this.marque = marque;
+      this.modele = modele;
+      this.longueur = longueur;
+      this.nbPlace = nbPlace;
+      this.reservoir = reservoir;
+      this.vitesse = vitesse;
+    }
+  }
+  
+  const avion1 = new Avion('Airbus', 'a380', 100, 700, 20000, 900);
+
+
+
+get marque() {
+    return this._marque;
+  }
+
+  set marque(marque) {
+    this._marque = marque;
+  }
+
+  get modele() {
+    return this._modele;
+  }
+  set modele(modele) {
+    this._modele = modele;
+  }
+
+  get longueur() {
+    return this._longueur;
+  }
+  set longueur(longueur) {
+    this._longueur = longueur;
+  }
+
+  get nbPlace() {
+    return this._nbPlace;
+  }
+  set nbPlace(nbPlace) {
+    this._nbPlace = nbPlace;
+  }
+
+  get reservoir() {
+    return this._reservoir;
+  }
+  set reservoir(reservoir) {
+    this._reservoir = reservoir;
+  }
+
+  get vitesse() {
+    return this._vitesse;
+  }
+  set vitesse(vitesse) {
+    this._vitesse = vitesse;
+  }
