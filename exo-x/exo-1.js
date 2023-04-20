eb691479d92819bf4a5cee718810fe2012ac979b
ehossard <etiennehossard@gmail.com>
2023-04-20 08:32:36 +0200

    exo-1

---
+++ exo-x/exo-1.js
@@ -0,0 +1,12 @@
+class Avion {
+  constructor(marque, modele, longueur, nbPlace, reservoir, vitesse) {
+    this.marque = marque;
+    this.modele = modele;
+    this.longueur = longueur;
+    this.nbPlace = nbPlace;
+    this.reservoir = reservoir;
+    this.vitesse = vitesse;
+  }
+}
+
+const avion1 = new Avion('Airbus', 'a380', 100, 700, 20000, 900);
